ddaf8c488590f5bd33cc8ffaf5852cfff00200ff
visualstudio <visualstudio@230-PC32>
2019-02-07 11:55:45 -0500

    final commit

--- CSD2214-section5-week5/js/main.js
+++ CSD2214-section5-week5/js/main.js
@@ -1,1 +1,25 @@
 ﻿// Your code here!
+function myFunction(a,b) {
+
+    return a+b;
+}
+function arrays() {
+    var pens;
+    /*pens = ["red", "orange", "green", "blue", "purple"];
+    console.log(pens);
+    var i;
+    for (i = 0; i < pens.length; i++) {
+        alert(pens[i]);  
+    }
+    alert("Length of array is : "+i);
+    */
+    pens = new Array("red", "orange", "green", "blue", "purple");
+    //markers = pens;
+    console.log(pens);
+    console.log(pens[0]);
+    var i;
+    for (i = 0; i < pens.length; i++) {
+        alert(pens[i]);
+    }
+    alert("Length of array is : " + i);
+}
